a47620f6be1fc37c7f51667ea62f957f92af300b
unknown <202080505@psu.palawan.edu.ph>
2022-10-11 20:10:25 +0800

    changed database credentials

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', '');
-	define('DB_HOST', '127.0.0.1');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'ramon422');
-	define('DB_NAME', 'practice5_06');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525739');
+	define('DB_PASS', 'yH2L8dExqP');
+	define('DB_NAME', 'sql6525739');
